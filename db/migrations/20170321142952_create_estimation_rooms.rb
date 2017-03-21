3431594f344c5bf4b00f592feb6dd17691ac647a
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-03-21 15:37:22 +0100

    Modified estimation room migration

--- db/migrations/20170321142952_create_estimation_rooms.rb
+++ db/migrations/20170321142952_create_estimation_rooms.rb
@@ -3,6 +3,11 @@ Hanami::Model.migration do
     create_table :estimation_rooms do
       primary_key :id
 
+      column :name, String
+      column :description, String, size: 10_000
+      column :admin_uuid, String, unique: true
+      column :voting_uuid, String, unique: true
+
       column :created_at, DateTime, null: false
       column :updated_at, DateTime, null: false
     end
