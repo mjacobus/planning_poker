8be35041153dc4025bef869622463832bd39b2e0
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-03-23 17:34:33 +0100

    WIP on serializers

---
+++ spec/serializers/estimation_room_serializer_spec.rb
@@ -0,0 +1,31 @@
+RSpec.describe EstimationRoomSerializer do
+  let(:attributes) do
+    fake_attributes(
+      :id,
+      :name,
+      :description,
+      :admin_uuid,
+      :voting_uuid,
+      :created_at,
+      :updated_at
+    )
+  end
+
+  let(:room) { double(attributes) }
+
+  subject { described_class.new(room) }
+
+  it 'inherits from EntitySerializer' do
+    expect(subject).to be_a(EntitySerializer)
+  end
+
+  describe '#to_h' do
+    it 'hides admin_uuid' do
+      expected = EstimationRoomForAdminsSerializer.new(room).to_h
+      expected.delete(:admin_uuid)
+
+      expect(subject.to_h).to eq(expected)
+    end
+  end
+end
+
