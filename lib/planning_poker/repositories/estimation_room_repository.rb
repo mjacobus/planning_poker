033bacee0e985b2e1d972ed1413954b9deb0536c
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-03-23 16:25:07 +0100

    WIP

--- lib/planning_poker/repositories/estimation_room_repository.rb
+++ lib/planning_poker/repositories/estimation_room_repository.rb
@@ -1,5 +1,15 @@
 class EstimationRoomRepository < Hanami::Repository
+  associations do
+    has_many :estimation_stories, foreign_key: :room_id
+  end
+
   def find_by_admin_uuid(admin_uuid)
-    estimation_rooms.where(admin_uuid: admin_uuid).limit(1).first
+    with_stories.where(admin_uuid: admin_uuid).limit(1).first
+  end
+
+  protected
+
+  def with_stories
+    aggregate(:estimation_stories)
   end
 end
