ba017e23557f921824a4709534a1044f216b9804
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-03-23 16:59:31 +0100

    Wip

--- spec/planning_poker/repositories/estimation_room_repository_spec.rb
+++ spec/planning_poker/repositories/estimation_room_repository_spec.rb
@@ -11,13 +11,15 @@ RSpec.describe EstimationRoomRepository do
 
       story1
       story2
+      factory.create_story(room_id: factory.create_room.id)
     end
 
     it 'returns a room with its stories' do
       admin_room = subject.find_by_admin_uuid(room.admin_uuid)
+      expect(admin_room.admin_uuid).to eq room.admin_uuid
 
-      expect(admin_room).to respond_to(:estimation_stories)
-      expect(admin_room.estimation_stories.length).to be(2)
+      expect(admin_room).to respond_to(:stories)
+      expect(admin_room.stories.length).to be(2)
     end
   end
 end
