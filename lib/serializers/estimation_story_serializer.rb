ff3384f6d861a7ce8149801485e3bcea08361d17
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-03-23 18:35:56 +0100

    Added tasks and stuff for development

--- lib/serializers/estimation_story_serializer.rb
+++ lib/serializers/estimation_story_serializer.rb
@@ -1,5 +1,12 @@
 class EstimationStorySerializer < EntitySerializer
   def to_h
-    {}
+    {
+      id: object.id,
+      name: object.name,
+      description: object.description,
+      created_at: object.created_at,
+      updated_at: object.updated_at,
+      room_id: object.room_id,
+    }
   end
 end
