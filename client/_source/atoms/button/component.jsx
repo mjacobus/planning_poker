7f0755ee67625b4390e1f45e167c20e54c0d9bb3
Nico Thiebes <nico.thiebes@xing.com>
2017-03-23 15:30:47 +0100

    Button pending addition

--- client/_source/atoms/button/component.jsx
+++ client/_source/atoms/button/component.jsx
@@ -3,15 +3,17 @@ import './index.scss';
 
 export default class Button extends Component {
   render() {
-    const { rank, className, onClick, text, type } = this.props;
+    const { rank, className, onClick, text, type, pending } = this.props;
     const rankClasses = {
       '1': 'button--primary',
       '2': 'button--secondary'
     };
+    const pendingClass = pending ? 'button--pending' : '';
 
     return (
-      <button type={ type } className={ ['button', className, rankClasses[rank]].join(' ') } onClick={ onClick }>
-        { text }
+      <button type={ type } className={ ['button', className, pendingClass, rankClasses[rank]].join(' ') } onClick={ onClick }>
+        <span className="button__text">{ text }</span>
+        <span className="button__busy">{ 'busy...' }</span>
       </button>
     );
   }
@@ -22,12 +24,14 @@ Button.propTypes = {
   text: PropTypes.string,
   type: PropTypes.string,
   onClick: PropTypes.func,
-  rank: PropTypes.number
+  rank: PropTypes.number,
+  pending: PropTypes.bool
 };
 
 Button.defaultProps = {
   className: '',
   text: '',
   type: 'text',
-  rank: 1
+  rank: 1,
+  pending: false
 };
