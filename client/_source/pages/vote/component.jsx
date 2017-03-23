6195ec617f62e7c4f304e7ea3f39743a3f90ca85
Nico Thiebes <nico.thiebes@xing.com>
2017-03-23 16:18:55 +0100

    Voting page

--- client/_source/pages/vote/component.jsx
+++ client/_source/pages/vote/component.jsx
@@ -8,11 +8,40 @@ export default class Vote extends Component {
   render() {
     const { name, description, roomName } = this.props;
 
+    // for testing
+    const users = [{
+      name: 'Marcelo',
+      estimation: null,
+      status: 'in_progress'
+    }, {
+      name: 'Nico',
+      estimation: 5,
+      status: 'finished'
+    }, {
+      name: 'Oliver',
+      estimation: 8,
+      status: 'finished'
+    }];
+    let usersFinishedCount = 0;
+
+    users.map((user) => {
+      if (user.status === 'finished') {
+        usersFinishedCount++;
+      }
+    });
+
+    const votes = `(${usersFinishedCount} of ${users.length} votes)`;
+
     return (
       <Page className="vote">
-        <div>{ roomName }</div>
+        <div className="vote__room">{ roomName }</div>
         <Heading text={ name } />
-        <Subheading text={ description } />
+        <Subheading className="vote__subheading" text={ description } />
+        <Heading className="vote__vote-now" text="Please vote now!" />
+        <div>{ votes }</div>
+        <ul className="vote__users">
+          {users.map((user, index) => <Heading text={ user.name } key={ index } className={ `vote__user vote__user--${user.status}` } />)}
+        </ul>
       </Page>
     );
   }
@@ -21,5 +50,6 @@ export default class Vote extends Component {
 Vote.propTypes = {
   name: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  roomName: PropTypes.string.isRequired
+  roomName: PropTypes.string.isRequired,
+  users: PropTypes.array.isRequired
 };
