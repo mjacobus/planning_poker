86fb502d1ba47c119fd6cd6e8d064d7c6fa94e54
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-03-23 18:06:28 +0100

    WIP

--- apps/api/controllers/estimation_rooms/show.rb
+++ apps/api/controllers/estimation_rooms/show.rb
@@ -3,6 +3,12 @@ module Api::Controllers::EstimationRooms
     include Api::Action
 
     def call(params)
+      admin_uuid = params[:admin_uuid]
+
+      room = EstimationRoomService.new.find_by_admin_uuid!(admin_uuid)
+
+      serializer = EstimationRoomForAdminsSerializer.new(room, :estimation_room)
+      json_response(serializer)
     end
   end
 end
--- spec/api/controllers/estimation_rooms/show_spec.rb
+++ spec/api/controllers/estimation_rooms/show_spec.rb
@@ -1,11 +1,32 @@
 require_relative '../../../../apps/api/controllers/estimation_rooms/show'
 
 RSpec.describe Api::Controllers::EstimationRooms::Show do
+  let(:factory) { TestFactory.new }
   let(:action) { described_class.new }
-  let(:params) { Hash[] }
+  let(:params) { Hash[admin_uuid: 'the_admin_uuid'] }
+  let(:room) { factory.create_room(admin_uuid: 'the_admin_uuid') }
+  let(:json_body) { { foo: :bar }.to_json }
 
-  it 'is successful' do
-    response = action.call(params)
-    expect(response[0]).to eq 200
+  before do
+    factory.rooms.clear
+  end
+
+  describe 'on a successful request' do
+    before do
+      room
+      stub_serializer(EstimationRoomForAdminsSerializer, room, :estimation_room) do
+        json_body
+      end
+    end
+
+    subject { action.call(params) }
+
+    it 'responds with json' do
+      expect(subject).to be_json
+    end
+
+    it 'responds with correct json' do
+      expect(subject).to have_json_body(json_body)
+    end
   end
 end
